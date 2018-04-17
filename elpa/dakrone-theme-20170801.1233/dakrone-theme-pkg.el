eaef278e955bdb4bf5bb89ec92831d6d127092c0
Varun Ramani <varun.ramani@gmail.com>
2018-04-17 17:22:23 -0400

    Taking a break from this in order to give Spacemacs another try

---
+++ elpa/dakrone-theme-20170801.1233/dakrone-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dakrone-theme" "20170801.1233" "dakrone's custom dark theme" 'nil :commit "232ad1be5f3572dcbdf528f1655109aa355a6937" :keywords '("color" "themes") :url "https://github.com/dakrone/dakrone-theme")
--- init.el
+++ init.el
@@ -1,5 +1,8 @@
 ;; Varun's Emacs Configuration
 
+;; Saving Emacs Startup time
+(setq gc-cons-threshold 100000000)
+
 ;; Packages
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
@@ -21,6 +24,9 @@
 (electric-pair-mode)
 (use-package evil :init (evil-mode) :ensure t)
 
+;; Line Numbers
+(global-linum-mode t)
+
 ;; Font
 (add-to-list 'default-frame-alist '(font . "SauceCodePro Nerd Font"))
 
@@ -29,7 +35,10 @@
 (use-package monokai-theme :ensure t :defer t)
 (use-package zenburn-theme :ensure t :defer t)
 (use-package color-theme-sanityinc-tomorrow :ensure t :defer t)
-(load-theme 'molokai t)
+(use-package dracula-theme :ensure t :defer t)
+(use-package alect-themes :ensure t :defer t)
+(use-package solarized-theme :ensure t :defer t)
+(load-theme 'solarized-dark t)
 
 ;; Spaceline
 (use-package spaceline
@@ -58,8 +67,6 @@
 
 
 
-
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -72,11 +79,13 @@
  '(beacon-color "#cc6666")
  '(custom-safe-themes
    (quote
-    ("ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" "b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default)))
+    ("e269026ce4bbd5b236e1c2e27b0ca1b37f3d8a97f8a5a66c4da0c647826a6664" "ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" "b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default)))
  '(fci-rule-color "#373b41")
  '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
  '(frame-background-mode (quote dark))
- '(package-selected-packages (quote (spaceline dracula-theme rebecca-theme use-package)))
+ '(package-selected-packages
+   (quote
+    (alect-themes dakrone-theme counsel spaceline dracula-theme rebecca-theme use-package)))
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
    (quote
