adf947e8a76fc926335e49d4b11053de05a61384
Varun Ramani <varun.ramani@gmail.com>
2018-04-20 09:34:22 -0400

    Started getting Ivy working

---
+++ elpa/popup-20160709.729/popup-autoloads.el
@@ -0,0 +1,15 @@
+;;; popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("popup.el") (23257 917 393276 18000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; popup-autoloads.el ends here
--- init.el
+++ init.el
@@ -22,13 +22,14 @@
 ;; Text Editing
 (setq tab-width 4)
 (electric-pair-mode)
+(use-package rainbow-delimiters :init (add-hook 'prog-mode-hook 'rainbow-delimiters-mode) :ensure t)
 (use-package evil :init (evil-mode) :ensure t)
 
 ;; Line Numbers
 (global-linum-mode t)
 
 ;; Font
-(add-to-list 'default-frame-alist '(font . "SauceCodePro Nerd Font"))
+(add-to-list 'default-frame-alist '(font . "FuraCode Nerd Font"))
 
 ;; Themes
 (use-package molokai-theme :ensure t :defer t)
@@ -38,6 +39,7 @@
 (use-package dracula-theme :ensure t :defer t)
 (use-package alect-themes :ensure t :defer t)
 (use-package solarized-theme :ensure t :defer t)
+(use-package material-theme :ensure t :defer t)
 (load-theme 'solarized-dark t)
 
 ;; Spaceline
@@ -52,59 +54,65 @@
 (use-package yasnippet-snippets :ensure t :defer t)
 (yas-global-mode)
 
+;; Autocomplete
+(use-package company
+  :init (company-mode)
+  :ensure t)
+
+;; Python Development
+(use-package elpy
+  :ensure t
+  :init (elpy-enable))
+
+;; Gradle
+(use-package gradle-mode
+  :defer t
+  :ensure t)
+(use-package groovy-mode
+  :defer t
+  :ensure t)
+(add-to-list 'auto-mode-alist '("\\.gradle\\'" . groovy-mode))
+;; Homescreen
+(use-package page-break-lines :ensure t)
+(use-package dashboard
+  :ensure t
+  :init
+  (dashboard-setup-startup-hook)
+  :config
+  (setq dashboard-banner-logo-title "Varun Ramani's Emacs Configuration")
+  (setq dashboard-startup-banner 'logo))
+
+
+;; Growing some Ivy
+(use-package counsel
+  :ensure t
+  :defer t)
+(use-package ivy
+  :ensure t)
+
+;; Projectile
+(use-package projectile
+  :ensure t
+  :defer t)
+
+
 ;; Disable GUI trash
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
 (tool-bar-mode -1)
-
-
-
-
-
-
-
-
-
-
+(set-window-fringes nil 0 0)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default bold shadow italic underline bold bold-italic bold])
- '(ansi-color-names-vector
-   (vector "#373b41" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#8abeb7" "#c5c8c6"))
- '(beacon-color "#cc6666")
- '(custom-safe-themes
-   (quote
-    ("e269026ce4bbd5b236e1c2e27b0ca1b37f3d8a97f8a5a66c4da0c647826a6664" "ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" "b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default)))
- '(fci-rule-color "#373b41")
- '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
- '(frame-background-mode (quote dark))
  '(package-selected-packages
    (quote
-    (alect-themes dakrone-theme counsel spaceline dracula-theme rebecca-theme use-package)))
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#cc6666")
-     (40 . "#de935f")
-     (60 . "#f0c674")
-     (80 . "#b5bd68")
-     (100 . "#8abeb7")
-     (120 . "#81a2be")
-     (140 . "#b294bb")
-     (160 . "#cc6666")
-     (180 . "#de935f")
-     (200 . "#f0c674")
-     (220 . "#b5bd68")
-     (240 . "#8abeb7")
-     (260 . "#81a2be")
-     (280 . "#b294bb")
-     (300 . "#cc6666")
-     (320 . "#de935f")
-     (340 . "#f0c674")
-     (360 . "#b5bd68"))))
- '(vc-annotate-very-old-color nil))
+    (material-theme zenburn-theme yasnippet-snippets use-package spaceline solarized-theme rebecca-theme projectile monokai-theme molokai-theme evil dracula-theme diminish dashboard dakrone-theme counsel color-theme-sanityinc-tomorrow arjen-grey-theme alect-themes))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
