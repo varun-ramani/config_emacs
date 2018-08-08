69e9ff034ccf2ece0a71161e25108812a41466c4
Varun Ramani <varun.ramani@gmail.com>
2018-08-08 10:01:25 -0400

    Added support for React and React Native development

--- init.el
+++ init.el
@@ -100,13 +100,23 @@
 (add-hook 'c-mode-common-hook #'lsp-cquery-enable)
 
 ;; JSX
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
-(add-hook 'web-mode-hook
-          (lambda ()
-            (when (string-equal "jsx" (file-name-extension buffer-file-name))
-              (setup-tide-mode))))
-(use-package tide :ensure t)
+(use-package json-mode :ensure t)
+(use-package rjsx-mode :ensure t
+  :config
+  (setq js2-basic-offset 4)
+  (setq typescript-indent-level 4)
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . rjsx-mode)))
+(defun setup-tide-mode ()
+  "Set up Tide mode."
+  (interactive)
+  (tide-setup)
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1))
+(use-package tide
+  :ensure t
+  :config
+  (setq company-tooltip-align-annotations t)
+  (add-hook 'rjsx-mode-hook #'setup-tide-mode))
 
 ;; Homescreen
 (use-package page-break-lines :ensure t)
