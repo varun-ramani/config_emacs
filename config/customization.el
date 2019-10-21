4bd0d130bae38e9d2a415b5728f7d2f01ebae465
Varun Ramani <varun.ramani@gmail.com>
2019-10-21 18:47:06 -0400

    Nicer block cursors

--- config/customization.el
+++ config/customization.el
@@ -2,11 +2,10 @@
 (use-package doom-themes :ensure t :defer t)
 (use-package afternoon-theme :ensure t :defer t)
 (use-package spacemacs-theme :ensure t :defer t)
-(use-package color-theme-sanityinc-tomorrow)
 
 (load-theme 'doom-one t)
 (doom-themes-neotree-config)
 
 ;; Font
-(add-to-list 'default-frame-alist '(font . "SauceCodePro Nerd Font 17"))
+(add-to-list 'default-frame-alist '(font . "Operator Mono 14"))
 (setq frame-resize-pixelwise t)
