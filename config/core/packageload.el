53e2650ef469ec280fb00d27e94b07db38afc8df
Varun Ramani <varun.ramani@gmail.com>
2019-10-21 18:46:50 -0400

    Removed GPG signature check

--- config/core/packageload.el
+++ config/core/packageload.el
@@ -1,8 +1,10 @@
 ;; Packages
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+(setq package-check-signature nil)
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
